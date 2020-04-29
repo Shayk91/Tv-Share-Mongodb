3c6ec000412e44b34b04b7091b1dcfee7965d3e3
Shay Kelley <shay@shays-macbook.lan>
2020-04-29 12:30:28 -0400

    heroku

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,85 @@
-const Show = require('../models/show');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+const Show = require('../models/show')
+const db = require('../db')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const SALT_ROUNDS = 11
+const TOKEN_KEY = 'areallylonggoodkey'
+
+const signUp = async (req, res) => {
+  try {
+    const { username, email, password } = req.body
+    const password_digest = await bcrypt.hash(password, SALT_ROUNDS)
+    const user = await new User({
+      username,
+      email,
+      password_digest
+    })
+
+    await user.save()
+
+    const payload = {
+      id: user._id,
+      username: user.username,
+      email: user.email
+    }
+
+    const token = jwt.sign(payload, TOKEN_KEY)
+    return res.status(201).json({ user, token })
+  } catch (error) {
+    console.log(
+      'You made it to the signUp controller, but there was an error :('
+    )
+    return res.status(400).json({ error: error.message })
+  }
+}
+
+const signIn = async (req, res) => {
+  try {
+    const { username, password } = req.body
+    const user = await User.findOne({ username: username })
+    if (await bcrypt.compare(password, user.password_digest)) {
+      const payload = {
+        id: user._id,
+        username: user.username,
+        email: user.email
+      }
+
+      const token = jwt.sign(payload, TOKEN_KEY)
+      return res.status(201).json({ user, token })
+    } else {
+      res.status(401).send('Invalid Credentials')
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+const verifyUser = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1]
+    const legit = jwt.verify(token, TOKEN_KEY)
+    if (legit) {
+      res.json({ user })
+    }
+  } catch (e) {
+    res.status(401).send('Not Authorized')
+  }
+}
+
+const changePassword = async (req, res) => { }
+
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find()
+    res.json(products)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
 
 const createShow = async (req, res) => {
   try {
@@ -65,6 +146,10 @@ const deleteShow = async (req, res) => {
 }
 
 module.exports = {
+  signUp,
+  signIn,
+  verifyUser,
+  changePassword,
   createShow,
   getAllShows,
   getShowById,
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,17 @@ const controllers = require('../controllers')
 const router = Router();
 
 router.get('/', (req, res) => res.send('This is root!'))
+
 router.get('/shows', controllers.getAllShows)
 router.post('/shows', controllers.createShow)
 router.get('/shows/:id', controllers.getShowById)
 router.put('/shows/:id', controllers.updateShow)
 router.delete('/shows/:id', controllers.deleteShow)
+
+router.post('/sign-up', controllers.signUp)
+router.post('/sign-in', controllers.signIn)
+router.get('/verify', controllers.verifyUser)
+router.post('/change-password', controllers.changePassword)
+
 
 module.exports = router;
